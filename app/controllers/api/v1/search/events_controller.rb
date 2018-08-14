e855a82b8d290da06ec09df2563685a9f110f8cd
Ben <benjaminpreed@gmail.com>
2018-08-14 12:58:43 -0400

    Event weather on tiles

--- app/controllers/api/v1/search/events_controller.rb
+++ app/controllers/api/v1/search/events_controller.rb
@@ -24,6 +24,22 @@ class Api::V1::Search::EventsController < ApplicationController
       date_trim = event["EventDate"][6...19]
       date = Time.at(date_trim.to_i/1000).to_s
       if date > Time.now
+        geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{event["EventAddress"].gsub(' ', '+')},#{event["EventCity"].gsub(' ', '+')},#{event["EventState"]}&key=AIzaSyAe7LWAeEng4tHXeKuMfYi9VLxvOZl4Ffo"
+        geo_data = JSON.parse(open(geocode_url).read)
+
+        event["EventLat"] = geo_data["results"][0]["geometry"]["location"]["lat"]
+        event["EventLng"] = geo_data["results"][0]["geometry"]["location"]["lng"]
+
+        date = date_trim[0..9]
+        weather_url = "https://api.darksky.net/forecast/472dcf776617a2bddc4edb7095c2547c/#{event["EventLat"]},#{event["EventLng"]},#{date}"
+
+        weather_data = JSON.parse(open(weather_url).read)
+
+        event["EventWeatherHigh"] = weather_data["daily"]["data"][0]["temperatureHigh"]
+        event["EventWeatherLow"] = weather_data["daily"]["data"][0]["temperatureLow"]
+        event["EventWeatherSummary"] = weather_data["daily"]["data"][0]["summary"]
+        event["EventPrecipProb"] = weather_data["daily"]["data"][0]["precipProbability"]
+        
         event_results << event
       end
     end
