06a5e4cff76f5fcdf7ec49f047c23b87ce983ff7
Ben <benjaminpreed@gmail.com>
2018-08-20 10:07:34 -0400

    Pending out test that relies on secret API key

--- spec/controllers/api/v1/events/events_controller_spec.rb
+++ spec/controllers/api/v1/events/events_controller_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe Api::V1::Events::EventsController, type: :controller do
   describe "POST#create" do
-    it "should return a single event with lat / long & weather forecast" do
+    xit "should return a single event with lat / long & weather forecast" do
       event = { "EventId"=>39397, "BTId"=>857, "EventName"=>"Tour of Nantucket", "EventAddress"=>"", "EventCity"=>"Siasconset", "EventState"=>"MA", "EventDate"=>"2018-08-12T04:00:00.000Z", "EventUrl"=>"http://www.BikeReg.com/tourofnantucket", "latitude"=>"41.2620995991908", "longitude"=>"-69.9731776711269", "RegOpenDate"=>"2018-07-01T04:15:00.000Z", "RegCloseDate"=>"2018-08-10T16:00:00.000Z", "EventTypes"=>["Road Race"] }
 
       get :create, params: event
